5e9832158e906f6316c8d53b572135d85088745f
João Paulo <79548287+jotapeee@users.noreply.github.com>
2021-10-06 18:58:32 -0300

    Add files via upload

--- src/main/java/AlunoIterator.java
+++ src/main/java/AlunoIterator.java
@@ -2,27 +2,14 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class AlunoIterator {
-    public static void main(String[] args) {
+    public void a() {
         ArrayList<Aluno> listaAluno = new ArrayList<Aluno>();
         Aluno aluno = null;
-
         for(int i=0; i >10; i++) {
             listaAluno.add(new Aluno("Aluno" + i,"endereço"+i,i));
 
-
         }
 
-        System.out.println();
-        System.out.println();
-
-        Iterator<Aluno> itrAluno = listaAluno.iterator();
-
-        while(itrAluno.hasNext()) {
-            aluno = (Aluno) itrAluno.next();
-            System.out.println("Nome:"+ aluno.getNome());
-            System.out.println("endereco:"+ aluno.getEndereco());
-            System.out.println("matricula:"+ aluno.getMatricula());
-        }
     }
 
 }
